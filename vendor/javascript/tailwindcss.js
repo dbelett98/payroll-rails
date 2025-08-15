248bf5a67917c45d459f15a4a749f81e1277edd5
dbelett98 <dbelett@outlook.com>
2025-08-14 22:02:24 -0500

    generated Devise views and styled login page with TailwindCSS

---
+++ vendor/javascript/tailwindcss.js
@@ -0,0 +1,4 @@
+// tailwindcss@4.1.12 downloaded from https://ga.jspm.io/npm:tailwindcss@4.1.12/dist/lib.mjs
+
+export{P as Features,d as Polyfills,V as __unstable__loadDesignSystem,$ as compile,m as compileAst,g as default}from"../_/DqwnFr4K.js";import"../_/CpDZGej_.js";import"../_/Cay67bh1.js";
+
