1aa3da2832829d5b5edc625cbc3aba742b313f6d
dbelett98 <dbelett@outlook.com>
2025-08-16 18:25:44 -0500

    .

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'psb-blue': '#1E3A8A',
+        'psb-purple': '#8B5CF6',
+        'psb-yellow': '#FBBF24',
+        'psb-teal': '#14B8A6'
+      }
+    }
+  },
+  plugins: []
+}
