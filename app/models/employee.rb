ef7ea962ae1abd4c8f3b8536b15a8fc64cebb11c
dbelett98 <dbelett@outlook.com>
2025-08-19 12:52:17 -0500

    fixed syntax error

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,8 +4,8 @@ class Employee < ApplicationRecord
   has_many :payroll_runs, dependent: :destroy
 
   validates :name, presence: true
-  validates :hours_worked, numericality: { greater_than_or_equal to: 0 }, allow_nil: true
-  validates :salary, numericality: { greater_than_or_equal to: 0 }, allow_nil: true
+  validates :hours_worked, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :salary, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 
   def calculate_pay
     (hours_worked || 0) * (salary || 0) / 2080.0  # Annual salary to hourly rate approximation
