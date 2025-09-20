2fa92561731429c43a3ce7a3ba48b1a2b214d26f
dbelett98 <dbelett@outlook.com>
2025-09-20 12:34:54 -0500

    payroll

--- app/services/payroll_calculation_service.rb
+++ app/services/payroll_calculation_service.rb
@@ -33,30 +33,113 @@ class PayrollCalculationService
     }
   end
 
-  # We'll implement these methods in subsequent parts
+private
+
+  def employee_summary
+    {
+      name: employee.name,
+      id: employee.id,
+      employment_type: employee.employment_type,
+      pay_frequency: employee.pay_frequency,
+      marital_status: employee.marital_status
+    }
+  end
+
   def calculate_gross_pay
-    # Part 2: Gross pay calculation
-    0.0
+    case employee.employment_type
+    when 'W2'
+      if employee.salary && employee.salary > 0
+        calculate_salary_gross_pay
+      else
+        calculate_hourly_gross_pay
+      end
+    when '1099'
+      calculate_hourly_gross_pay # 1099 workers are typically hourly
+    else
+      0.0
+    end
+  end
+
+  def calculate_salary_gross_pay
+    return 0.0 unless employee.salary
+
+    case employee.pay_frequency
+    when 'weekly'
+      employee.salary / 52.0
+    when 'biweekly'
+      employee.salary / 26.0
+    when 'semimonthly'
+      employee.salary / 24.0
+    when 'monthly'
+      employee.salary / 12.0
+    else
+      employee.salary / 26.0 # default to biweekly
+    end
+  end
+
+  def calculate_hourly_gross_pay
+    overtime_info = calculate_overtime
+    regular_pay = overtime_info[:regular_hours] * (employee.hourly_rate || 0)
+    overtime_pay = overtime_info[:overtime_hours] * overtime_info[:overtime_rate]
+    regular_pay + overtime_pay
   end
 
   def calculate_overtime
-    # Part 2: Overtime calculation
-    { regular_hours: 0, overtime_hours: 0, overtime_rate: 0 }
+    return { regular_hours: 0, overtime_hours: 0, overtime_rate: 0 } unless employee.hourly_rate
+
+    # Convert total hours to pay period hours
+    pay_period_hours = hours_for_pay_period
+    regular_hours_limit = regular_hours_limit_for_period
+
+    if pay_period_hours <= regular_hours_limit
+      {
+        regular_hours: pay_period_hours,
+        overtime_hours: 0,
+        overtime_rate: employee.hourly_rate * 1.5
+      }
+    else
+      {
+        regular_hours: regular_hours_limit,
+        overtime_hours: pay_period_hours - regular_hours_limit,
+        overtime_rate: employee.hourly_rate * 1.5
+      }
+    end
   end
 
-  def calculate_federal_taxes
-    # Part 3: Federal tax calculation
-    { withholding: 0.0, social_security: 0.0, medicare: 0.0 }
+  def hours_for_pay_period
+    # If specific pay period hours provided, use those
+    return @pay_period_hours if @pay_period_hours
+
+    # Otherwise convert from stored hours_worked based on pay frequency
+    total_hours = employee.hours_worked || 0
+    
+    case employee.pay_frequency
+    when 'weekly'
+      total_hours / 4.0  # Assuming monthly storage
+    when 'biweekly'
+      total_hours / 2.0  # Assuming monthly storage
+    when 'semimonthly'
+      total_hours / 2.0
+    when 'monthly'
+      total_hours
+    else
+      total_hours / 2.0  # default to biweekly
+    end
   end
 
-  def calculate_state_taxes
-    # Part 4: State tax calculation
-    { withholding: 0.0, sdi: 0.0, sui: 0.0 }
-  end
-
-  def calculate_deductions
-    # Part 5: Deductions calculation
-    { health_insurance: 0.0, retirement_401k: 0.0, other: 0.0 }
+  def regular_hours_limit_for_period
+    case employee.pay_frequency
+    when 'weekly'
+      40.0
+    when 'biweekly'
+      80.0
+    when 'semimonthly'
+      86.67  # (40 * 52) / 24
+    when 'monthly'
+      173.33 # (40 * 52) / 12
+    else
+      80.0  # default to biweekly
+    end
   end
 
   def calculate_net_pay
